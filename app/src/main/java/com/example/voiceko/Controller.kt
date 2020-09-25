5acfd4826b8bfe839ad570ff46b4459e7839da20
RskerZ <coplwear@gmail.com>
2020-09-25 19:20:39 +0800

    the Main Activity and pussy

---
+++ app/src/main/java/com/example/voiceko/Controller.kt
@@ -0,0 +1,4 @@
+package com.example.voiceko
+
+class Controller {
+}
--- app/src/main/java/com/example/voiceko/MainActivity.kt
+++ app/src/main/java/com/example/voiceko/MainActivity.kt
@@ -1,22 +1,51 @@
 package com.example.voiceko
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    var CTR = Controller()
+    var date = Calendar.getInstance()
+    private lateinit var rePortBtn: Button
+    private lateinit var AAABtn: Button
+    private lateinit var incomeText: TextView
+    private lateinit var payText: TextView
+    private lateinit var totalText: TextView
+    private lateinit var monthText: TextView
+    private lateinit var recordList: ListView
+    var testlist = arrayListOf<String>("A","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C","B","C")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var btn1:Button = findViewById<Button>(R.id.button)
-        btn1.setOnClickListener(ttt)
+        rePortBtn = findViewById<Button>(R.id.rePortBtn)
+        AAABtn = findViewById<Button>(R.id.AAABtn)
+        incomeText = findViewById<TextView>(R.id.incomeText)
+        payText = findViewById<TextView>(R.id.payText)
+        totalText = findViewById<TextView>(R.id.totalText)
+        recordList = findViewById<ListView>(R.id.record)
+        monthText = findViewById<TextView>(R.id.monthView)
+        rePortBtn.setOnClickListener(testIncome)
+        AAABtn.setOnClickListener(change)
+        monthText.text = date.get(Calendar.YEAR).toString() + "年" + (date.get(Calendar.MONTH)+1).toString() + "月"
+        totalText.text = (incomeText.text.toString().toInt() - payText.text.toString().toInt()).toString()
+    }
+    private var testIncome = View.OnClickListener {
+        var adaper = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,testlist)
+        recordList.adapter = adaper
+    }
+    private var change = View.OnClickListener {
 
     }
-    var ttt = View.OnClickListener {
+    /*var testIncome = View.OnClickListener {
         Toast.makeText(this,"fuck",Toast.LENGTH_LONG).show()
     }
+    */
+
 
 }
