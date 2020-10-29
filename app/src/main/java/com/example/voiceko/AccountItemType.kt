4f7ceb3b251212d8cbb8830dbe184054b0aa3922
RskerZ <coplwear@gmail.com>
2020-10-29 08:40:10 +0800

    fixed cost complete 70%

--- app/src/main/java/com/example/voiceko/AccountItemType.kt
+++ app/src/main/java/com/example/voiceko/AccountItemType.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class AccountItemType: Fragment() {
+class AccountItemType(var actname: String): Fragment() {
+    private var editTextType:TextView? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v: View = inflater.inflate(R.layout.fragment_accounttype, container, false)
         return v
@@ -14,6 +16,10 @@ class AccountItemType: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        editTextType = activity!!.findViewById(R.id.editType)
     }
+
+    fun showtype(type: TextView){
+    }
+
 }
--- app/src/main/java/com/example/voiceko/FixCostActivity.kt
+++ app/src/main/java/com/example/voiceko/FixCostActivity.kt
@@ -1,63 +1,73 @@
 package com.example.voiceko
 
 import android.app.DatePickerDialog
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
 import android.widget.Switch
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import java.util.*
 
-
-class EnterData : AppCompatActivity() {
+class FixCostActivity : AppCompatActivity() {
     val c: Calendar = Calendar.getInstance()
     var mYear = c.get(Calendar.YEAR)
     var mMonth = c.get(Calendar.MONTH)
     var mDay = c.get(Calendar.DAY_OF_MONTH)
-    val lilcaculater: Fragment = LilCaculater("Enter")
-    val accItem: Fragment = AccountItemType("Enter")
-    private lateinit var editTextDate: TextView
+    val lilcaculater: Fragment = LilCaculater("Fixedcost")
+    val accItem: Fragment = AccountItemType("Fixedcost")
     private lateinit var toolbar: Toolbar
+    private lateinit var editTextDate: TextView
     private lateinit var editTextNumber: TextView
     private lateinit var editTextType: TextView
     private lateinit var editTextSubType: TextView
     private lateinit var switchType: Switch
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_enter_data)
-        editTextDate = findViewById<TextView>(R.id.editTextDate)
-        toolbar = findViewById(R.id.enter_toolbar)
-        editTextNumber = findViewById(R.id.editTextNumber)
-        editTextType = findViewById(R.id.editType)
-        editTextSubType = findViewById(R.id.editSubType)
-        switchType = findViewById(R.id.switch1)
+        setContentView(R.layout.activity_fix_cost)
+        editTextDate = findViewById<TextView>(R.id.fixedcost_editTextDate)
+        toolbar = findViewById(R.id.fixedcost_toolbar)
+        editTextNumber = findViewById(R.id.fixedcost_editTextNumber)
+        editTextType = findViewById(R.id.fixedcost_editType)
+        editTextSubType = findViewById(R.id.fixedcost_editSubType)
+        switchType = findViewById(R.id.fixedcost_switch)
 
-        editTextNumber.setOnClickListener(editNumber)
-        editTextDate.setOnClickListener(editDate)
-        editTextType.setOnClickListener(editType)
+        editTextNumber.setOnClickListener {
+            editTextNumber.text = ""
+            showFragment("lil")
+        }
+        editTextDate.setOnClickListener {
+            DatePickerDialog(this, { _, mYear, mMonth, mDay ->
+                run {
+                    val format = "${setDateFormat(mYear, mMonth, mDay)}"
+                    editTextDate.text = format
+                }
+            }, mYear, mMonth, mDay).show()
+        }
+        editTextType.setOnClickListener {
+            showFragment("acc")
+        }
+
+
         //設定今天的日期
         editTextDate.text = "${mYear}/${mMonth+1}/${mDay}"
+
         //收入支出切換
         switchType.setOnCheckedChangeListener{ _, isCheck->
             if(isCheck) {//如果按開關，可以用此按鈕來改變是收入還是支出(若之後要編輯記帳紀錄可以用個TAG之類的東西紀錄他是支出還是收入，在Oncreate的時候就對switch按鍵進行變動)
-                toolbar.setTitle("收入")
+                toolbar.setTitle("固定收入")
             }else{
-                toolbar.setTitle("支出")
+                toolbar.setTitle("固定支出")
             }
         }
 
         //工具列，設置返回鍵啟用
         setSupportActionBar(toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
-
     }
-
     override fun onBackPressed() {
         super.onBackPressed()
         val ft = supportFragmentManager.beginTransaction()
@@ -68,39 +78,17 @@ class EnterData : AppCompatActivity() {
     //返回鍵
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId == android.R.id.home){
-                finish()
+            finish()
         }
         return super.onOptionsItemSelected(item)
     }
 
-    //日期選擇器
-    private var editDate = View.OnClickListener {
 
-        DatePickerDialog(this, { _, mYear, mMonth, mDay ->
-            run {
-                val format = "${setDateFormat(mYear, mMonth, mDay)}"
-                editTextDate.text = format
-            }
-        }, mYear, mMonth, mDay).show()
-    }
-
+    //function
     private fun setDateFormat(year: Int, month: Int, day: Int): String {
         return "$year/${month + 1}/$day"
     }
 
-    //計算機
-    private var editNumber = View.OnClickListener {
-        editTextNumber.text = ""
-        showFragment("lil")
-
-    }
-    private var editType = View.OnClickListener {
-        showFragment("acc")
-    }
-
-
-
-    //function
     fun showFragment(f: String){
         val ft = supportFragmentManager.beginTransaction()
         hideFragment(ft)
