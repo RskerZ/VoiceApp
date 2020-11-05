031c6fa7f580bae523bf36ae94cfb666f18ca1ed
liaosboy <nick87101227@gmail.com>
2020-11-05 09:18:09 +0800

    load record by sqlite

--- app/src/main/java/com/example/voiceko/MainActivity.kt
+++ app/src/main/java/com/example/voiceko/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.voiceko
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
-import android.widget.*
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.voiceko.Controller.EnterDataController
-import com.example.voiceko.DataBase.ConsumptionRecordContract
+import com.example.voiceko.Controller.RecordController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -19,44 +26,118 @@ class MainActivity : AppCompatActivity() {
     private lateinit var totalText: TextView
     private lateinit var recordList: ListView
     private lateinit var toolbar: Toolbar
+    private lateinit var controller:RecordController
+
+
     val c: Calendar = Calendar.getInstance()
 
     var mYear = c.get(Calendar.YEAR)
     var mMonth = c.get(Calendar.MONTH)
-    private lateinit var testlist :ArrayList<String>
+    private lateinit var recordlist :ArrayList<ArrayList<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var dbmgr = ConsumptionRecordContract.DBMgr(this)
-        testlist = dbmgr.readRecord()
-        incomeText = findViewById(R.id.incomeText)
-        payText = findViewById(R.id.payText)
-        totalText = findViewById(R.id.totalText)
-        recordList = findViewById(R.id.record)
-
-        toolbar = findViewById(R.id.main_toolbar)
+        init()
         // 設定右上角的 menu
         toolbar.inflateMenu(R.menu.selectmonth)
-        toolbar.title = "${mYear}年${mMonth + 1}月"
+        toolbar.setOnClickListener(editDate)
+        toolbar.setOnMenuItemClickListener(selectMonthListener)
 
         val bottomNavigationView =
             findViewById(R.id.menuBtn) as BottomNavigationView
         bottomNavigationView.menu.setGroupCheckable(0, false, false)
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomBarListener)
 
-        bottomNavigationView.setOnNavigationItemSelectedListener { item: MenuItem ->
-            when (item.itemId) {
-                R.id.addMenu -> goEnter()
-                R.id.micMenu -> {
-                    Toast.makeText(this, "你好我會語音辨識", Toast.LENGTH_SHORT).show()
-                }
-                R.id.reportMenu -> goChart()
-                R.id.setMenu -> goSetting()
-            }
-            true
-        }
         //載入記帳紀錄
-        setRecord(testlist)
+        load_info()
+
+    }
+
+
+    var selectMonthListener= Toolbar.OnMenuItemClickListener{
+        when(it.itemId){
+            R.id.rightBtn -> {
+                nextMonth()
+                load_info()
+                true
+            }
+            R.id.leftBtn -> {
+                lastMonth()
+                load_info()
+                true
+            }
+            else ->{
+                true
+            }
+        }
+    }
+
+    var bottomBarListener = BottomNavigationView.OnNavigationItemSelectedListener{item: MenuItem ->
+        when (item.itemId) {
+            R.id.addMenu -> goEnter()
+            R.id.micMenu -> {
+                Toast.makeText(this, "你好我會語音辨識", Toast.LENGTH_SHORT).show()
+            }
+            R.id.reportMenu -> goChart()
+            R.id.setMenu -> goSetting()
+        }
+        true
+    }
+
+    private var editDate = View.OnClickListener {
+
+        var mDay=15
+
+        DatePickerDialog(this, { _, mYear, mMonth, D ->
+            run {
+                this.mMonth = mMonth
+                this.mYear = mYear
+                mDay = D
+                load_info()
+            }
+        },mYear , mMonth, mDay).show()
+    }
+
+
+    fun nextMonth(){
+        if (mMonth == 11){
+            mMonth = 0
+            mYear++
+        }else{
+            mMonth++
+        }
+    }
+
+    fun lastMonth(){
+        if (mMonth == 0){
+            mMonth = 11
+            mYear--
+        }else{
+            mMonth--
+        }
+    }
+
+
+    fun init(){
+        controller = RecordController(this)
+        incomeText = findViewById(R.id.incomeText)
+        payText = findViewById(R.id.payText)
+        totalText = findViewById(R.id.totalText)
+        recordList = findViewById(R.id.record)
+        toolbar = findViewById(R.id.main_toolbar)
+
+
+    }
+
+    fun load_info(){
+        var listToShow = arrayListOf<String>()
+        recordlist = controller.loadRecordList(mYear,mMonth)
+        for (record in recordlist){
+            listToShow.add(record[1])
+        }
+        toolbar.title = "${mYear}年${mMonth + 1}月"
+        setRecord(listToShow)
     }
 
     //切換Activity
@@ -81,15 +162,6 @@ class MainActivity : AppCompatActivity() {
         recordList.adapter = adapter
     }
 
-
-
-
-
-
-    /*var testIncome = View.OnClickListener {
-        Toast.makeText(this,"fuck",Toast.LENGTH_LONG).show()
-    }
-    */
 
 
 }
